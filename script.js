74de1dc35f90d91057111f2bc84bccaef45f7275
lets travel <george@brewsystems.com>
2020-03-30 17:24:33 -0400

    getting started html and css

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+});
+    console.log(moment().format('LT'));
+    $(".hour").text(moment().format('dddd LT'));
+    $(".date").text(moment().format('MMMM DD YYYY'));
