49a8a2be9af410d354dbaad905d32bf2b1583673
lets travel <george@brewsystems.com>
2020-04-10 22:24:17 -0400

    got calls working UV index running

--- weather.js
+++ weather.js
@@ -1,41 +1,119 @@
-$(document).ready(function () {
+window.onload = () => {
 
     console.log(moment().format('LT'));
+    let todaysDate = moment().format("L");
     $(".hour").text(moment().format('dddd LT'));
     $(".date").text(moment().format('MMMM DD YYYY'));
-    var APIKey = "c9a4470d4efaf75504fc4a5aa2627f1f";
-    var locationArray = [];
+    let apiKey = "c9a4470d4efaf75504fc4a5aa2627f1f";
+    let locationArray = JSON.parse(localStorage.getItem("location")) || [];
+    // let fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + apiKey + "&units=imperial";
 
-    $("#searchBTN1").on("click", function () {
-        var location = $("#locationInput").val().trim();
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=" + APIKey + "&units=imperial";
+    if (locationArray && locationArray.length > 0) {
+        while (locationArray.length > 3) {
+            locationArray.shift();
+        }
+        makeCall(locationArray[0]);
+    }
 
-        console.log(location);
+    $("#searchBTN1").on("click", (event) => {
+        event.preventDefault();
+        let location = $("#locationInput").val().trim();
+        console.log(location)
+        for (let i = 0; i < locationArray.length; i++) {
+            if (locationArray[i] === location) {
+                locationArray.splice(i, 1);
+            }
+        }
+
+        if (location != null) {
+
+            locationArray.push(location);
+            while (locationArray.length > 4) {
+                locationArray.shift();
+            }
+
+            localStorage.setItem("locationArray", JSON.stringify(locationArray));
+        }
+
+        makeCall();
+    })
+
+    const makeCall = () => {
+        getWeather();
+        // fiveDay();    
+        displayHistory();
+    }
+
+
+    const displayHistory = () => {
+        console.log(locationArray);
+        for (let i = 0; i < locationArray.length; i++) {
+            $("#favBTN" + (i)).text(locationArray[i]).removeClass("hide");
+        }
+    }
+
+   getWeather=()=>{
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationArray[0] + "&APPID=" + apiKey + "&units=imperial";
+
+
         $.ajax({
+
             url: queryURL,
             method: "GET"
+
         })
+            .then((response)=> {
 
-            .then(function (response) {
-                console.log(response);
+                $("#todaysWeather").empty();
 
-            });
 
-    })
-    $("#searchBTN2").on("click", function () {
-        var location = $("#locationInput").val().trim();
-        var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + APIKey + "&units=imperial";
+                let iconcode = response.weather[0].icon;
+                let iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+                let uvLat = response.coord.lat;
+                let uvLon = response.coord.lon;
+                indexUV(uvLon, uvLat);
 
-        console.log(location);
+                $("#todaysWeather").append($("<div>").attr("id", "todayStats"));
+                $("#todayStats").append(`<h2>${response.name}<img src = ${iconurl}></h2>
+                <h3>Temperature : ${(response.main.temp).toFixed(0)}&#176;F</h3>
+                    <h4>Wind Speed : ${response.wind.speed} mph</h4>
+                    <h4>Humidity :${response.main.humidity}%</h4>
+                    <h4 id='uv'></h4>`
+                );
+
+            })
+    }
+
+    indexUV=(uvLon, uvLat)=> {
+        let queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=15e701943db0eab65638c75f992c9b15&lat=" + uvLat + "&lon=" + uvLon;
+
         $.ajax({
-            url: fiveDayQueryURL,
+
+            url: queryURL,
             method: "GET"
+
         })
+            .then((response) => {
 
-            .then(function (response) {
-                console.log(response);
+                let uvIndex = response.value;
+                $("#uv").text("UV Index : " + uvIndex);
+                if (uvIndex < 3) {
+                    $("#uv").addClass("green");
+                }
+                if (uvIndex > 3 && uvIndex < 6) {
+                    $("#uv").addClass("yellow");
+                }
+                if (uvIndex > 6 && uvIndex < 8) {
+                    $("#uv").addClass("orange");
+                }
+                if (uvIndex > 8 && uvIndex < 11) {
+                    $("#uv").addClass("red");
+                }
+                if (uvIndex > 11) {
+                    $("#uv").addClass("purple");
+                }
 
-            });
+            })
+    }
 
-    })
-});
+};
